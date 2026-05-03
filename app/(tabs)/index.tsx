162ae874e858388156f53942ec23a8b158b3d693
Sharifah33 <your-email@example.com>
2026-05-03 11:10:33 +1000

    first commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,40 @@
+import * as Location from 'expo-location';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  const [location, setLocation] = useState<any>(null);
+
+  const getLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+
+    if (status !== 'granted') {
+      alert('Permission denied');
+      return;
+    }
+
+    let loc = await Location.getCurrentPositionAsync({});
+    setLocation(loc.coords);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Button title="Get My Location" onPress={getLocation} />
+
+      {location && (
+        <Text>
+          Latitude: {location.latitude} {'\n'}
+          Longitude: {location.longitude}
+        </Text>
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
